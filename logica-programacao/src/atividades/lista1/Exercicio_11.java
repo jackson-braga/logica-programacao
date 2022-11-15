adb7cea00a9bdca7fda0a0f1ba0e95ab0af35759
Eduardo Alexandre <luis.edu.alex@gmail.com>
2022-11-15 14:41:47 -0300

    Finaliza Exercício 11 da Lista de Exercícios 1

--- logica-programacao/src/atividades/lista1/Exercicio_11.java
+++ logica-programacao/src/atividades/lista1/Exercicio_11.java
@@ -1,4 +1,33 @@
 package atividades.lista1;
 
-//public class Exercicio_11 {
-//}
+import java.util.Scanner;
+
+public class Exercicio_11 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Entrada de Dados
+        System.out.println("Programa que verifica quantidade em estoque, e necessidade de compra.");
+        System.out.println("Informe a quantidade de produtos em estoque: ");
+        int emEstoque = scan.nextInt();
+        System.out.println("Informe a quantidade mínima de produtos prevista para o estoque: ");
+        int minima = scan.nextInt();
+        System.out.println("Informe a quantidade máxima de produtos prevista para o estoque: ");
+        int maxima = scan.nextInt();
+
+        //Processamento de Dados
+        int media = (minima + maxima) / 2;
+        String mensagem;
+        if ( emEstoque >= media) {
+            mensagem = "Não efetuar compra.";
+        }
+        else {
+            mensagem = "Efetuar comprar.";
+        }
+
+        //Saída de Dados
+        System.out.println(mensagem);
+    }
+}
