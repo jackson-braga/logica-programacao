fe14179f8ffb4486814d43dfcfa04f4b3af54038
Marcos V <marcosvinicius@employer.com.br>
2022-11-15 02:10:03 -0300

    Lista 02, em andamento

--- logica-programacao/src/atividades/lista2/Exercicio2.java
+++ logica-programacao/src/atividades/lista2/Exercicio2.java
@@ -1,4 +1,58 @@
 package atividades.lista2;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
+    /**
+     * Escreva um programa para ler o número de lados de um polígono regular e a medida do lado (em cm). Calcular e imprimir o
+     * seguinte:
+     * ● Se o número de lados for igual a 3 escrever TRIÂNGULO e o valor da área
+     * ● Se o número de lados for igual a 4 escrever QUADRADO e o valor da sua área.
+     * ● Se o número de lados for igual a 5 escrever PENTÁGONO.
+     * ● se o número de lados for inferior a 3 escrever NÃO É UM POLÍGONO.
+     * ● se o número de lados for superior a 5 escrever POLÍGONO NÃO IDENTIFICADO.
+     */
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        System.out.print("Informe a quantidade de lados do polígono: ");
+        int qLados = scann.nextInt();
+        double[] lados = new double[qLados];
+        /** usando foreach */
+        int cont = 0;
+        for (double lado: lados) {
+            System.out.print("Informe o " + (cont + 1) + "º lado do polígono em cm: ");
+            lados[cont] = scann.nextDouble();
+            cont++;
+        }
+        /** usando for */
+//        for (int i = 0; i <= qLados - 1; i++) {
+//            System.out.print("Informe o " + (i + 1) + "º lado do polígono em cm: ");
+//            lados[i] = scann.nextDouble();
+//        }
+        //calcula área
+        double area = 0;
+        for (int i = 0; i < lados.length; i++) {
+            //usando ternário
+            area = area == 0 ? lados[i] :  area * lados[i];
+        }
+        if (qLados == 3) {
+            System.out.println("\nTRIÂNGULO");
+            System.out.println("A área total é de: " + area + " cm².");
+        } else if (qLados == 4) {
+            System.out.println("\nQUADRADO");
+            System.out.println("A área total é de: " + area + " cm².");
+        }
+        if (qLados == 5) {
+            System.out.println("\nPENTÁGONO");
+            System.out.println("A área total é de: " + area + " cm².");
+        }
+        if (qLados < 3) {
+            System.out.println("\nNÃO É UM POLÍGONO");
+        }
+        if (qLados > 5) {
+            System.out.println("\nPOLÍGONO NÃO IDENTIFICADO");
+        }
+
+    }
 }
+
--- logica-programacao/src/atividades/lista2/Exercicio3.java
+++ logica-programacao/src/atividades/lista2/Exercicio3.java
@@ -1,4 +1,32 @@
 package atividades.lista2;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
+    /**
+     * Escreva um programa que leia as medidas dos lados de um triângulo e escreva se ele é Equilátero, Isósceles ou Escaleno.
+     * Sendo que:
+     * ● Triângulo Equilátero: possui os 3 lados iguais.
+     * ● Triângulo Isóscele: possui 2 lados iguais.
+     * ● Triângulo Escaleno: possui 3 lados diferentes
+     */
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        System.out.print("Informe o primeiro lado (cm): ");
+        int ladoUm = scann.nextInt();
+        System.out.print("Informe o segundo lado (cm): ");
+        int ladoDois = scann.nextInt();
+        System.out.print("Informe o terceiro lado (cm): ");
+        int ladoTres = scann.nextInt();
+
+        if (ladoUm == ladoDois && ladoUm == ladoTres && ladoDois == ladoTres) {
+            System.out.println("Triângulo Equilátero!");
+        } else if (ladoUm == ladoDois || ladoUm == ladoTres || ladoDois == ladoTres) {
+            System.out.println("Triângulo Isóceles!");
+        } else {
+            System.out.println("Triângulo Escaleno!");
+            System.out.println("Triângulo Escaleno!");
+            System.out.println("Triângulo Escaleno!");
+        }
+    }
 }
