163f0d0dff869be4134e0c720ceb1bb57fc423b9
Wandemberg <86700373+WandembergSS@users.noreply.github.com>
2022-11-15 21:54:14 -0300

    Create Q1.java

---
+++ logica-programacao/src/atividades/lista1/Q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        double base = scan.nextDouble();
+        double altura = scan.nextDouble();
+
+        System.out.println("A area é: " + base*altura);
+
+
+    }
+}
