fa32b38f90f448e9756b047ac08e2ace49d2d04a
Wandemberg <86700373+WandembergSS@users.noreply.github.com>
2022-11-16 11:10:05 -0300

    Create Q10.java

---
+++ logica-programacao/src/atividades/lista1/Q10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Qual o salário por hora?");
+        double salario = scan.nextDouble();
+        System.out.println("Qual a quantidade de horas trabalhadas no mês?");
+        double horas = scan.nextDouble();
+
+        if (horas>  160){
+            System.out.println("Salário total: " + (salario*(160 + 1.5*horas - 240)) );
+        } else {
+            System.out.println("Salário total: " + (salario*horas) );
+        }
+
+    }
+}
