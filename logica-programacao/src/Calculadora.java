d32e7f9df215b19e9554204d42365a2c02383029
Joel <joelvieirasouza@gmail.com>
2022-11-16 22:17:29 -0300

    adicao classe calculadora

--- logica-programacao/src/Calculadora.java
+++ logica-programacao/src/Calculadora.java
@@ -1,89 +1,37 @@
-import java.util.Scanner;
-
 public class Calculadora {
-    int index;
+    public double calcular(double valor1, double valor2, int indexOp) {
 
-    public static void main(String[] args) {
-        // Entrada de dados
-//        double valor1 = Double.valueOf(args[0]);
-//        double valor2 = Double.valueOf(args[1]);
+        
+        double resultado = 0;
 
-        int indexOp;
-        Scanner scann = new Scanner(System.in);
-        do {
-            System.out.println("Escolha uma das operações abaixo:");
-            System.out.println("1 - Soma");
-            System.out.println("2 - Subtração");
-            System.out.println("3 - Multiplicação");
-            System.out.println("4 - Divisão");
-            System.out.println("0 - Sair");
-
-            indexOp = scann.nextInt();
-
-            if (indexOp < 0 || indexOp > 4) {
-                System.out.println("\nOperação inexistente!\n");
-                continue;
-//                break;
-            }
-
-            System.out.println("Informe o primeiro número:");
-            double valor1 = scann.nextDouble();
-            System.out.println("Informe o segundo número:");
-            double valor2 = scann.nextDouble();
-
-            double resultado = 0;
-//        if (indexOp == 0) {
-//            // Soma
-//            resultado = valor1 + valor2;
-//        } else if (indexOp == 1) {
-//            // subtração
-//            resultado = valor1 - valor2;
-//        } else if (indexOp == 2) {
-//            // multiplicação
-//            resultado = valor1 * valor2;
-//        } else if (indexOp == 3) {
-//            // divisão
-//            resultado = valor1 / valor2;
-//        }
-
-            switch (indexOp) {
-                case 1:
-                    resultado = valor1 + valor2;
-                    break;
-                case 2:
-                    resultado = valor1 - valor2;
-                    break;
-                case 3:
-                    resultado = valor1 * valor2;
-                    break;
-                case 4:
-                    resultado = valor1 / valor2;
-                    break;
-                default:
-                    break;
-            }
-
-            // Saída de dados
-            System.out.println("Resultado: " + resultado);
-
-//        while(indexOp != 0){
-//            indexOp = 1;
-//        }
-//
-//        do {
-//            indexOp = 0;
-//        } while(indexOp != 0);
-//
-//        double[] valores = new double[] {1,2};
-//        for (int i = 0; i < valores.length; i++) {
-//            double valor = valores[i];
-//        }
-//
-//        for (double valor : valores) {
-//
-//        }
-        } while (indexOp != 0);
-
-        System.out.println("\nFinalizando Calculadora!");
+        switch (indexOp) {
+            case 1:
+                resultado = somar(valor1, valor2);
+                break;
+            case 2:
+                resultado = subtrair(valor1, valor2);
+                break;
+            case 3:
+                resultado = multiplicar(valor1, valor2);
+                break;
+            case 4:
+                resultado = dividir(valor1, valor2);
+                break;
+            default:
+                break;
+        }
+        return resultado;
+    }
+    private double somar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+    private double subtrair(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+    private double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+    private double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
     }
 }
--- logica-programacao/src/CalculadoraMain.java
+++ logica-programacao/src/CalculadoraMain.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class CalculadoraMain {
+import java.util.Scanner;
+
+public class CalculadoraMain {
+
+    public static void main(String[] args) {
+        int indexOp;
+
+        Scanner scann = new Scanner(System.in);
+        do {
+
+            indexOp = getOperacao();
+
+            if (indexOp < 0 || indexOp > 4) {
+                System.out.println("\nOperação inexistente!\n");
+                continue;
+//                break;
+
+            } else if (indexOp != 0) {
+
+                Calculadora calc = new Calculadora();
+                System.out.println("Informe o primeiro número:");
+                double valor1 = scann.nextDouble();
+                System.out.println("Informe o segundo número:");
+                double valor2 = scann.nextDouble();
+
+                double resultado = calc.calcular(valor1, valor2, indexOp);
+
+                System.out.println("Resultado: " + resultado);
+            }
+
+        } while (indexOp != 0);
+
+        System.out.println("\nFinalizando Calculadora!");
+
+        scann.close();
+
+    }
+    private static int getOperacao() {
+        Scanner scann = new Scanner(System.in);
+
+        System.out.println("Escolha uma das operações abaixo:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.println("0 - Sair");
+
+        int indexOp = scann.nextInt();
+        scann.close();
+        return indexOp;
+    }
+
 }
+
+
