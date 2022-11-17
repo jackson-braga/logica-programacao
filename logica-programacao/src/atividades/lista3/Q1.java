24ce07b0331a1b84d71d8862ca0086d16776679d
Wandemberg <86700373+WandembergSS@users.noreply.github.com>
2022-11-17 20:35:04 -0300

    Create Q1.java

---
+++ logica-programacao/src/atividades/lista3/Q1.java
@@ -0,0 +1,12 @@
+public class Q1 {
+
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 1000; i <= 2000; i++){
+            if (i%11 == 2){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
