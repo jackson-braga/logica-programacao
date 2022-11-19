0bcd2c31f68c2cd15725ca6039c61c2e1fa6643b
kdinizf <karina.dinizf@gmail.com>
2022-11-19 17:01:16 -0300

    Iniciando exercícios

---
+++ logica-programacao/src/atividades/lista1/ExeUm.java
@@ -0,0 +1,25 @@
+package atividades.lista1;
+import java.util.Scanner;
+public class ExeUm {
+    public static void main(String[] args) {
+
+        int base;
+        int altura;
+        int area;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+
+        System.out.println("Digite o valor da base do retângulo: ");
+        base = scanner.nextInt();
+
+        System.out.println("Digite o valor da altura do retângulo: ");
+        altura = scanner.nextInt();
+
+        area = base * altura;
+
+        System.out.println("A aréa do retângulo é: " + area);
+
+    }
+
+}
