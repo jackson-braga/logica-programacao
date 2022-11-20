0a534ba9903d2644fd4c46df2cbf64537bf79107
kdinizf <karina.dinizf@gmail.com>
2022-11-20 19:30:19 -0300

    Resolução de novos exercícios

--- logica-programacao/src/atividades/lista1/Exe14.java
+++ logica-programacao/src/atividades/lista1/Exe14.java
@@ -1,13 +1,62 @@
 package atividades.lista1;
 
+import java.util.Scanner;
+
 public class Exe14 {
     public static void main(String[] args) {
 
         String nomeProduto;
         int qtdProduto;
-        double valorUnidadeProduto;
+        double precoUnidadeProduto;
+        double valorTotalSemDesconto;
+        double valorTotalDesconto;
+        double valorTotalPagamento;
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Qual produto você comprou?");
+        nomeProduto = scanner.next();
+
+        System.out.println("Quantos?");
+        qtdProduto = scanner.nextInt();
+
+        System.out.println("Qual o valor unitário?");
+        precoUnidadeProduto = scanner.nextDouble();
+
+        valorTotalSemDesconto = precoUnidadeProduto * qtdProduto;
+
+        if (qtdProduto <= 5) {
+            valorTotalDesconto = valorTotalSemDesconto * 0.02;
+            valorTotalPagamento = valorTotalSemDesconto - valorTotalDesconto;
+            System.out.println("Você comprou " + qtdProduto + "x" + " " + nomeProduto + ".");
+            System.out.println("------------------------------------");
+            System.out.println("Valor total da compra: " + valorTotalSemDesconto + ";");
+            System.out.println("Valor total de desconto: " + valorTotalDesconto + ";");
+            System.out.println("Valor para pagamento: R$" + valorTotalPagamento + ".");
+
+        }
+
+        if (qtdProduto > 5 && qtdProduto <= 10) {
+            valorTotalDesconto = valorTotalSemDesconto * 0.03;
+            valorTotalPagamento = valorTotalSemDesconto - valorTotalDesconto;
+            System.out.println("Você comprou " + qtdProduto + "x" + " " + nomeProduto + ".");
+            System.out.println("------------------------------------");
+            System.out.println("Valor total da compra: " + valorTotalSemDesconto + ";");
+            System.out.println("Valor total de desconto: " + valorTotalDesconto + ";");
+            System.out.println("Valor para pagamento: R$" + valorTotalPagamento + ".");
+
+        }
+
+        if (qtdProduto > 10) {
+            valorTotalDesconto = valorTotalSemDesconto * 0.05;
+            valorTotalPagamento = valorTotalSemDesconto - valorTotalDesconto;
+            System.out.println("Você comprou " + qtdProduto + "x" + " " + nomeProduto + ".");
+            System.out.println("------------------------------------");
+            System.out.println("Valor total da compra: " + valorTotalSemDesconto + ";");
+            System.out.println("Valor total de desconto: " + valorTotalDesconto + ";");
+            System.out.println("Valor para pagamento: R$" + valorTotalPagamento + ".");
+
+        }
 
     }
 
---
+++ logica-programacao/src/atividades/lista1/Exe15.java
@@ -0,0 +1,10 @@
+package atividades.lista1;
+
+public class Exe15 {
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
